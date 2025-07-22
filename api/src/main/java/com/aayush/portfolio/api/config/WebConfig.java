ed5822d405f0399abdc5899ec927d6f0220d13e9
Aayush Shrestha <saayush97@gmail.com>
2025-07-22 18:05:00 -0300

    feat: (ci/cd) docker and k8s integration

--- api/src/main/java/com/aayush/portfolio/api/config/WebConfig.java
+++ api/src/main/java/com/aayush/portfolio/api/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Allow CORS for all API endpoints
-                .allowedOrigins("http://localhost:5173", "YOUR_FUTURE_RENDER_URL") // Allow your local dev server
+                .allowedOrigins("http://localhost:5173", "http://localhost") // Allow your local dev server
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
--- ui/src/services/apiService.ts
+++ ui/src/services/apiService.ts
@@ -1,7 +1,6 @@
-// File: ui/src/services/apiService.ts
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api/v1';
+const API_BASE_URL = "/api/v1"; 
 
 export const apiClient = axios.create({
     baseURL: API_BASE_URL,
